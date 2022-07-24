90fd9f04e941aeb30ddfa6c7e3efb6e8b0fa4ea3
Juninho <juninhobh96@hotmail.com>
2022-07-23 21:27:09 -0300

    Bot funcional

--- main.js
+++ main.js
@@ -14,9 +14,14 @@ const distube = new DisTube(client, {
 })
 const prefix = "'";
 
+const config = {
+    token: "ODk3MjIwNzMxMjgzMzI1MDI4.YWSf6w.sNi1Hkn-Ut6HT3g_mqX_Fcpx8ew",
+}
+
 client.once('ready', () => {
     console.log('MuseCaly is online');
 });
+
 
 client.on("messageCreate", message =>{
     if(message.author.bot) return;
@@ -24,11 +29,8 @@ client.on("messageCreate", message =>{
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift();
 
-    //Status da Queue de musicas
-    const status = (queue) => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Repeat: \`${queue.repeatMode ? queue.repeatMode == 2 ? "All Queue" : "Essa Musica" : "Off"}`;
-
     //Comandos para o Bot
-    if(command === "play" || "p"){
+    if((command === ("p")) || (command === ("play"))){
         if(!message.member.voice.channel) return message.textChannel.send('Você não está em um chat de voz!');
         if(!args[0]) return message.textChannel.send('Você precisar colocar algo para tocar!');
         distube.play(message, args.join(" "));
@@ -53,7 +55,44 @@ client.on("messageCreate", message =>{
         }
     }
 
-    if (command === "skip") distube.skip(message)
+
+    //if (command === "skip") distube.skip(message)
+    
+/*
+    
+    if (command === "skip") {
+
+        const queue = distube.getQueue(message)
+        if (!queue){
+
+            message.channel.send("Não existem musicas a serem tocadas. Adicione alguma!");
+        
+        }else{
+            if (!Queue.autoplay && Queue.song.length <= 1){
+                distube.skip(message);
+            }else{
+                distube.stop(message);
+            }
+        }
+        
+    }
+*/
+
+    if (command === "skip") {
+        
+        const queue = distube.getQueue(message)
+        
+        if (!queue){
+            message.channel.send("Não existem musicas a serem tocadas. Adicione alguma!");
+        }else{
+            if (queue.autoplay || queue.songs.length > 1){
+                distube.skip(message);
+            }else{
+                distube.stop(message);
+                message.channel.send("Fim da lista, adicione mais músicas!");
+            }  
+        }
+    }
 
     if (command === "resume") distube.resume(message)
 
@@ -64,39 +103,36 @@ client.on("messageCreate", message =>{
         const filter = distube.setFilter(message, command)
         message.channel.send(`Current queue filter: ${filter.join(", ") || "Off"}`)
     }
+});   
+
+//Status da Queue de musicas
+const status = (queue) => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Repeat: \`${queue.repeatMode ? queue.repeatMode == 2 ? "All Queue" : "Essa Musica" : "Off"}`;
+
+
+//Listeners para mandar mensagens dependendo do
+distube
+    .on("playSong", (queue, song) => queue.textChannel.send(
+        `Tocando \`${song.name} - \`${song.formattedDuration}\`\nSolicitado por: ${song.user.tag}\n${status(queue)}`
+    ))
     
-    //Listagem e comandos do bot
-    distube
-        .on("playSong", (queue, song) => queue.textChannel.send(
-            `Tocando \`${song.name} - \`${song.formattedDuration}\`\nSolicitado por: ${song.user.tag}\n${status(queue)}`
-        ))
-        
-        .on("addSong", (queue, song) => queue.textChannel.send(
-            `Adicionado \`${song.name} - \`${song.formattedDuration}\` a lista por ${song.user.tag}`
-        ))
-        
-        .on("playList", (queue, playlist, song) => queue.textChannel.send(
-            `Tocando as musicas da playlist \`${playlist.name}\` (${playlist.songs.length} ).\nSolicitado por: ${song.user.tag}\nTocando agora \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
-        ))
+    .on("addSong", (queue, song) => queue.textChannel.send(
+        `Adicionando \`${song.name} - \`${song.formattedDuration}\` a lista por ${song.user.tag}`
+    ))
+    
+    .on("playList", (queue, playlist, song) => queue.textChannel.send(
+        `Tocando as musicas da playlist \`${playlist.name}\` (${playlist.songs.length} ).\nSolicitado por: ${song.user.tag}\nTocando agora \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
+    ))
 
-        .on("addList", (queue, playlist) => queue.textChannel.send(
-            `Adicionando a playlist \`${playlist.name}\` (${playlist.songs.length} musicas) a lista\n${status(queue)}` 
-        ))
-        
-        /*.on("searchResult", (message, result) => {
-            let i = 0;
-            message.textChannel.send(`**Escolha uma opção abaixo**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n *aguarde`)
-        })
+    .on("addList", (queue, playlist) => queue.textChannel.send(
+        `Adicionando a playlist \`${playlist.name}\` (${playlist.songs.length} musicas) a lista\n${status(queue)}` 
+    ))
+    
+    .on("error", (message, e) => {
+        console.error(e)
+        message.textChannel.send("Um erro foi encontrado: " + e);
+    })
+    
 
-        .on("searchCancel", (message) => message.textChannel.send(`Pesquisa cancelada`))
-        */
-       
-        .on("error", (message, e) => {
-            console.error(e)
-            message.textChannel.send("Um erro foi encontrado: " + e);
-        })
-
-});
 
 //Login é a ultima coisa que deve estar no codigo
-client.login('ODk3MjIwNzMxMjgzMzI1MDI4.YWSf6w.q_rcgT43j04J4VemP_YU6wndl6U');
+client.login(config.token)
